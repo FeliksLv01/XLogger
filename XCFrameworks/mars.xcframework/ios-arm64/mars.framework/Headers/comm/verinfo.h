2b79f5f3b617a474a845a8152236f86de9a93f7a
FeliksLv <felikslv@163.com>
2025-10-09 01:07:41 +0800

    feat: update mars

--- XCFrameworks/mars.xcframework/ios-arm64/mars.framework/Headers/comm/verinfo.h
+++ XCFrameworks/mars.xcframework/ios-arm64/mars.framework/Headers/comm/verinfo.h
@@ -2,11 +2,11 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "88163033"
+#define MARS_REVISION "709250d6"
 #define MARS_PATH "feat/build_xlog_only"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2025-10-08 23:34:13"
+#define MARS_BUILD_TIME "2025-10-09 01:05:54"
 #define MARS_TAG ""
-#define MARS_BUILD_TIMESTAMP 1759937653
+#define MARS_BUILD_TIMESTAMP 1759943154
 
 #endif
